8a3a7be223cdc79f73c35ea56ca9613824b4b1f7
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-02-17 21:04:52 +0530

    Linear Search - Array

--- Demo.java
+++ Demo.java
@@ -1,5 +1,17 @@
-class Demo {
-    public static void main(String args[]){
-        System.out.print("Hello");
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] array = {1 ,2 , 3 ,4 , 5, 6 , 7 , 8 , 9 ,10};
+        System.out.print("Enter the element you are searching for : ");
+        int input = sc.nextInt();
+        for(int i = 0 ; i < array.length ; i++){
+            if(array[i] == input){
+                System.out.println(input + " is found at index " + i + ".");
+            }
+        }
     }
 }
+// whatever argument we will pass in terminal , that is called as Command Line Argument, here we have to pass the first element of array - args and that will be displayed as output.
+
